9732f43fac1fdbaa9c71585309b4e6c98b4b827b
stephengeller <stephen.geller@hotmail.com>
2018-06-03 14:33:08 +0100

    create mvp example nagios check

--- check_all_users_are_employed.sh
+++ check_all_users_are_employed.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 # TAKE IN PARAMETERS
-
+ENDPOINT=$1
+FULL_ENDPOINT=http://localhost:3000/${ENDPOINT}
 
 # EXECUTE LOGIC
-
+NOT_EMPLOYED_USERS=$(curl ${FULL_ENDPOINT} | jq ' .users[] | select(."isEmployed"==false)')
 
 # ASSESS RESULT
+if [[ ${NOT_EMPLOYED_USERS} ]]; then
+  echo 'User(s) not employed on account:'
+  echo ${NOT_EMPLOYED_USERS} | jq '"\(.name) - \(.id)"'
+  exit 1
+else
+  echo "All users are employed"
+  exit 0
+fi
--- routes/users.js
+++ routes/users.js
@@ -3,23 +3,88 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
-  users = [
-    {
-      name: "Stephen Geller",
-      id: 1,
-      isEmployed: true
-    },
-    {
-      name: "Steve Hayes",
-      id: 2,
-      isEmployed: true
-    },
-    {
-      name: "Jake Paul",
-      id: 1,
-      isEmployed: false
-    }
-  ];
+  users = {
+    users: [
+      {
+        name: "Stephen Geller",
+        id: 1,
+        isEmployed: true
+      },
+      {
+        name: "Steve Hayes",
+        id: 2,
+        isEmployed: true
+      },
+      {
+        name: "Jake Paul",
+        id: 3,
+        isEmployed: false
+      },
+      {
+        name: "Beyonce Knowles",
+        id: 4,
+        isEmployed: false
+      }
+    ]
+  };
+
+  res.send(users);
+});
+
+router.get("/passing", function(req, res, next) {
+  users = {
+    users: [
+      {
+        name: "Stephen Geller",
+        id: 1,
+        isEmployed: true
+      },
+      {
+        name: "Steve Hayes",
+        id: 2,
+        isEmployed: true
+      },
+      {
+        name: "Jake Paul",
+        id: 3,
+        isEmployed: true
+      },
+      {
+        name: "Beyonce Knowles",
+        id: 4,
+        isEmployed: true
+      }
+    ]
+  };
+
+  res.send(users);
+});
+
+router.get("/failing", function(req, res, next) {
+  users = {
+    users: [
+      {
+        name: "Stephen Geller",
+        id: 1,
+        isEmployed: true
+      },
+      {
+        name: "Steve Hayes",
+        id: 2,
+        isEmployed: true
+      },
+      {
+        name: "Jake Paul",
+        id: 3,
+        isEmployed: false
+      },
+      {
+        name: "Beyonce Knowles",
+        id: 4,
+        isEmployed: false
+      }
+    ]
+  };
 
   res.send(users);
 });
