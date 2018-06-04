6a47f5a94086fcd26e9f4a3fbfa85a93577b7bbc
Stephen Geller <stephen.geller@hotmail.com>
2018-06-04 12:04:23 +0100

    create test blah endpoint for future use

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({ isResponding: true});
+    res.send({ isResponding: true});
+});
+
+router.get('/blah', function(req, res, next) {
+    res.send({ isResponding: true, blah: 'yes'});
 });
 
 module.exports = router;
